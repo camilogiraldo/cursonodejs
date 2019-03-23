947458e01388da6cc55f3250d65e727b3ac2b446
Danielgiraldo <danielgiraldoch@gmail.com>
2019-03-23 12:42:49 -0500

    Ejercicio

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-#nuestro primer proyecto
+Primero instale los paquetes de node modules con npm instal
+Luego ejecutar el ejercicio con node principal Registrar y agregar los valores que se piden
---
+++ principal.js
@@ -0,0 +1,62 @@
+let cursos = [{
+		id: 1,
+		curso: 'Angular',
+		tiempo: 40,
+		precio: 100000
+	},
+	{
+		id: 2,
+		curso: 'Node js',
+		tiempo: 30,
+		precio: 50000
+	},
+	{
+		id: 3,
+		curso: 'Git hub',
+		tiempo: 20,
+		precio: 200000
+	}
+];
+
+let opciones = {
+	codigo: {
+		demand: true,
+		alias: 'i'
+	},
+	nombre: {
+		demand: true,
+		alias: 'n'
+	},
+	cedula: {
+		demand: true,
+		alias: 'c'
+	},
+}
+
+const express = require('express')
+const app = express()
+const fs = require('fs');
+const argv = require('yargs').command('Registrar', 'Realizar registro, escriba uno de los siguientes códigos: 1,2,3', opciones).argv
+
+if (cursos[argv.codigo]) {
+	console.log('Curso: ' + cursos[argv.codigo].curso + ', Duracion: ' + cursos[argv.codigo].tiempo + ', Horas Precio: ' + cursos[argv.codigo].precio);
+	console.log('Abra el navegador en la url localhost:3000');
+
+	var texto = '<h1>CURSO MATRICULADO</h1> <br> El estudiante: ' + argv.nombre + '<br>' + 'Con numero de identificacion: ' + argv.cedula + '<br>' + 'Se ha matriculado en el curso: ' + cursos[argv.codigo].curso +
+		'<br>' + 'Con una duracion de: ' + cursos[argv.codigo].tiempo + ' horas<br>' +
+		'El cual tiene un valor de: ' + cursos[argv.codigo].precio + ' pesos<br>';
+} else {
+	console.log('El codigo que ingresó no es valido');
+	console.log('Los cursos actuales son:');
+	console.log('Codigo: ' + cursos[0].id + ', Curso: ' + cursos[0].curso + ', precio:' + cursos[0].precio + ', Duracion: ' + cursos[0].tiempo);
+	console.log('Codigo: ' + cursos[1].id + ', Curso: ' + cursos[1].curso + ', precio:' + cursos[1].precio + ', Duracion: ' + cursos[1].tiempo);
+	console.log('Codigo: ' + cursos[2].id + ', Curso: ' + cursos[2].curso + ', precio:' + cursos[2].precio + ', Duracion: ' + cursos[2].tiempo);
+
+}
+
+app.get('/', function (req, res) {
+	res.send(texto)
+})
+
+app.listen(3000)
+
